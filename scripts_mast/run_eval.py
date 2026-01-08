9a2ebf70a61b2124ec176c46bf0f3e939a636bb0
Tobia Boschi <tobia.boschi@ibm.com>
2026-01-08 08:43:59 +0100

    simplified mast scripts

--- scripts_mast/run_eval.py
+++ scripts_mast/run_eval.py
@@ -14,13 +14,12 @@ from scripts.pipeline_tools.initialize_dataset_and_metadata import (
 from scripts_mast.mast_utils import (
     build_task_config,
     build_signals_by_role_from_task_config,
-)
-from scripts_mast.mast_utils.pipeline_helpers import (
     setup_device_and_mp,
     extract_signal_stats,
     build_default_transform,
     build_window_datasets,
     make_collate_fn,
+    DEFAULT_CONFIGS_ROOT,
 )
 
 from mmt.utils.config import (
@@ -71,7 +70,7 @@ def main() -> None:
     cfg_mmt = load_experiment_config(
         task=args.task,
         phase="eval",
-        configs_root="scripts_mast/configs",
+        configs_root=DEFAULT_CONFIGS_ROOT,
     )
     validate_config(cfg_mmt.raw)
 
